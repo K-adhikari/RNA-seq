554c2818bc3094ff6fad1f8b26b0925d15eac69f
K-adhikari <152343178+K-adhikari@users.noreply.github.com>
2025-09-08 21:30:28 -0400

    Update Head_DESeq2.R
    Fixed spacing in codes

--- Head_DESeq2.R
+++ Head_DESeq2.R
@@ -1,6 +1,7 @@
 #### #####Downstream processing of count data in R
 
 #reading data into R
+
 CSrab18H1<-read.table("htseq_CSrab-18C-H1.txt", header = FALSE, col.names = c("Gene", "CSrab18H1"))
 CSrab18H2<-read.table("htseq_CSrab-18C-H2.txt", header = FALSE, col.names = c("Gene", "CSrab18H2"))
 CSrab18H3<-read.table("htseq_CSrab-18C-H3.txt", header = FALSE, col.names = c("Gene", "CSrab18H3"))
@@ -14,7 +15,9 @@ IsoCS29H1<-read.table("htseq_IsoCS-29C-H1.txt", header = FALSE, col.names = c("G
 IsoCS29H2<-read.table("htseq_IsoCS-29C-H2.txt", header = FALSE, col.names = c("Gene", "IsoCS29H2"))
 IsoCS29H3<-read.table("htseq_IsoCS-29C-H3.txt", header = FALSE, col.names = c("Gene", "IsoCS29H3"))
 
+
 #merging data into a single table
+
 all.htseq__head<- merge(CSrab18H1, CSrab18H2, by = "Gene",all = TRUE)
 all.htseq__head<- merge(all.htseq__head, CSrab18H3, by = "Gene", all = TRUE)
 all.htseq__head<- merge(all.htseq__head, CSrab29H1, by = "Gene", all = TRUE)
@@ -27,55 +30,78 @@ all.htseq__head<- merge(all.htseq__head, IsoCS29H1, by = "Gene", all = TRUE)
 all.htseq__head<- merge(all.htseq__head, IsoCS29H2, by = "Gene", all = TRUE)
 all.htseq__head<- merge(all.htseq__head, IsoCS29H3, by = "Gene", all = TRUE)
 
+
 library(DESeq2)
 
 ## removing first five rows that contains data not being considered for analysis
 
 all.htseq__head<- all.htseq__head[-1:-5, ]
 
+
 ## Changing rownames
+
 rownames(all.htseq__head)<- all.htseq__head$Gene
 
+
 ## making a genotype array
+
 Genotype<- c(rep("IIIm", 6), rep("Ym", 6))
 
+
 ## making a temperature array
+
 Temperature<- c(rep("18C", 3), rep("29C", 3), rep("18C",3), rep("29C", 3))
 
+
 ## making a table containing samples, genotype and temperature
+
 exp.design<- data.frame(Genotype, Temperature, rownames = names(all.htseq__head[-c(1)]))
 
+
 ## DESeq analysis
+
 dds<-DESeqDataSetFromMatrix(as.matrix(all.htseq__head[,2:13]), colData = exp.design, design = ~ Genotype + Temperature + Genotype: Temperature)
 dds<- DESeq(dds)
 resultsNames(dds)
 
+
 ## Interaction  term (GxT): Temperature effect across genotypes 
+
 res1<- data.frame(results(dds, name="GenotypeYm.Temperature29C"))
 res_GT<- subset(res1, padj<0.05)
 write.csv(res_GT, "interaction_head.csv")
 
+
 ## Contrasting genotypes at 18C
+
 geno1<- results(dds, contrast = c("Genotype", "Ym", "IIIm"))
 geno1.df <- data.frame(geno1)
 geno_18C<- subset(geno1.df, padj<0.05)
 
+
 ## Contrasting genotypes at 29C
+
 geno2<- results(dds, list(c("Genotype_Ym_vs_IIIm", "GenotypeYm.Temperature29C")))
 geno2.df<- data.frame(geno2)
 geno_29C<- subset(geno2.df, padj<0.05)
 
+
 ## Effect of temperature on genotype IIIm
+
 res2<- results(dds, contrast = c("Temperature", "29C", "18C"))
 res2.df <- as.data.frame(res2)
 temp_IIIM<- subset(res2.df, padj<0.05)
 
+
 #Effect of temperature on genotype Ym
+
 res3<- results(dds, contrast =  list(c("Temperature_29C_vs_18C", "GenotypeYm.Temperature29C")))
 res3.df <- as.data.frame(res3)
 temp_YM<- subset(res3.df, padj<0.05)
 
+
 ## Plotting PCA
+
 rld<- rlog(dds)
 plotPCA(rld, intgroup= c("Temperature", "Genotype"), ntop=16540) ###Using all genes
 plotPCA(rld, intgroup= c("Temperature", "Genotype"), ntop=500)  ### Using top 500 genes
@@ -84,8 +110,10 @@ plotPCA(rld, intgroup= c("Temperature", "Genotype"), ntop=500)  ### Using top 50
 ## Heatmap of sample to sample distance
 
 #calculate the distance
+
 rld<- rlog(dds) 
 Dis<- dist(t(assay(rld)))
+
 
 #heatmap
 
@@ -109,15 +137,21 @@ pheatmap(sampleDistMatrix,
 resLFC <- lfcShrink(dds, coef="GenotypeYm.Temperature29C", type="ashr")
 plotMA(resLFC, ylim=c(-5,5), alpha=0.05)
 
+
 ### For Genotype effect at 18C
+
 geno1_LFC<- lfcShrink(dds, coef = "Genotype_Ym_vs_IIIm", type = "ashr")
 plotMA(geno1_LFC, ylim=c(-5,5), alpha=0.05)
 
+
 #### For Genotype effect at 29C
+
 geno2_LFC<- lfcShrink(dds, contrast= list(c("Genotype_Ym_vs_IIIm", "GenotypeYm.Temperature29C")),type = "ashr")
 plotMA(geno2_LFC, ylim=c(-5,5), alpha=0.05)
 
+
 #### For Temperature effect in IIIM
+
 res2_LFC<- lfcShrink(dds, coef ="Temperature_29C_vs_18C", type = "ashr")
 plotMA(res2_LFC, ylim=c(-5,5), alpha=0.05)
 
